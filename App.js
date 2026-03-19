6ab0e188a4b33b30d6d0f67c23ceac6ffcca2551
cpamporis <chrispamporis@gmail.com>
2026-03-19 20:15:55 +0200

    App.js

--- App.js
+++ App.js
@@ -1,16 +1,38 @@
-//App.js
 import "react-native-gesture-handler";
 import React from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Provider as PaperProvider, MD3LightTheme } from "react-native-paper";
 import { en, registerTranslation } from "react-native-paper-dates";
+import { useFonts } from "expo-font";
+
+import {
+  MaterialIcons,
+  FontAwesome5,
+  MaterialCommunityIcons,
+  Feather,
+  Entypo,
+} from "@expo/vector-icons";
+
 import RootApp from "./src/RootApp";
 import ErrorBoundary from "./src/components/ErrorBoundary";
 
 registerTranslation("en", en);
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    ...MaterialIcons.font,
+    ...FontAwesome5.font,
+    ...MaterialCommunityIcons.font,
+    ...Feather.font,
+    ...Entypo.font,
+  });
+
+  // ⛔ IMPORTANT: wait for fonts
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <PaperProvider theme={MD3LightTheme}>
       <SafeAreaProvider>
